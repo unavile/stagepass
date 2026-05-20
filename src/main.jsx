a239bdec2e653f1eeb3e7c88612cad3b6be6fe3e
unavile <uday.navile@gmail.com>
2026-05-20 14:09:11 -0400

    Fix: uninstall react-router-dom, fix hook ordering

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App'
-import Success from './Success'
 
-const isSuccessPage = window.location.pathname === '/success'
+function Test() {
+  return <div style={{ color: 'white', padding: 40 }}>React is working</div>
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {isSuccessPage ? <Success /> : <App />}
+    <Test />
   </StrictMode>
 )
