d85467d57267eae0f62f84683fa34b7d1c366f75
unavile <uday.navile@gmail.com>
2026-05-20 15:37:53 -0400

    Fix: add detailed logging

--- netlify/functions/stripe-webhook.js
+++ netlify/functions/stripe-webhook.js
@@ -4,7 +4,7 @@ const { createClient } = require('@supabase/supabase-js')
 exports.handler = async (event) => {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
   const supabase = createClient(
-    process.env.VITE_SUPABASE_URL,
+    process.env.SUPABASE_URL,
     process.env.SUPABASE_SERVICE_ROLE_KEY
   )
 
