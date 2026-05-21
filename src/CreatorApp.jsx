ae8cb0c219dcf2040ec6ba72ba2ddeb5f9baaaee
unavile <uday.navile@gmail.com>
2026-05-21 08:17:54 -0400

    Fix: remove eject_at_room_exp

--- src/CreatorApp.jsx
+++ src/CreatorApp.jsx
@@ -21,7 +21,7 @@ const TEXT3   = '#8c8883'
 // const TEXT3   = '#555250'
 
 // Background images
-// const IMG_STUDIO = 'https://images.unsplash.com/photo-1598488035139-bdbb2231ce04?w=1920&q=80'
+const IMG_STUDIO = 'https://images.unsplash.com/photo-1598488035139-bdbb2231ce04?w=1920&q=80'
 
 
 const card = (extra = {}) => ({
